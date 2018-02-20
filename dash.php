5053576edd5a4dfe2a54dd8a7465f0712b0d22c2
marcolcal <marcolcal@gmail.com>
2018-02-19 20:33:11 -0500

    Added logic to show the username of user after login

--- dash.php
+++ dash.php
@@ -5,9 +5,14 @@
 		header('location: index.php');
 	}
 	include("includes/nav.php");
+
+	$user_id = $_SESSION['user_id'];
+	$sql = "SELECT * FROM `users` WHERE `id`=".$user_id;
+	$res = mysql_query($sql);
+	$user = mysql_fetch_object($res);
 ?>
 <main>
-	
+	Welcome <?=$user->username ?>
 </main>
 <?php
 	include("includes/footer.php");
